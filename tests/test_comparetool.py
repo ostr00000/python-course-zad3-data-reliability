6b1df82475c3ba0a2c06efa1ae59e7fee16dc078
ostr00000 <ostr00000@gmail.com>
2017-06-04 17:44:15 +0200

    move tests

--- tests/test_comparetool.py
+++ tests/test_comparetool.py
@@ -1,17 +1,17 @@
-import articleSimilarity as asim
+import comparetool.articleSimilarity as comp
 
 
 def test_compare_strings():
     a = ['ala', 'ala']
-    [(_, _, val)] = asim.compare_strings(a)
+    [(_, _, val)] = comp.compare_strings(a)
     assert val == 1
 
     a = ['a', 'b', 'c']
-    [(_, _, a), (_, _, b), (_, _, c)] = asim.compare_strings(a)
+    [(_, _, a), (_, _, b), (_, _, c)] = comp.compare_strings(a)
     assert a == 0 and b == 0 and c == 0
 
     test_str = 'ala ma kota kot ma ale'.split()
-    ret = asim.compare_strings(test_str)
+    ret = comp.compare_strings(test_str)
     v = len(test_str)
     assert len(ret) == v * (v - 1) / 2
 
@@ -23,7 +23,7 @@ def test_rotate_tuple_list():
     l = [t1, t2, t3]
 
     expected = [(1, 3, 5), (2, 4, 6), ('ala', 'kot', 'pies')]
-    returned = asim.rotate_tuple_list(l)
+    returned = comp.rotate_tuple_list(l)
 
     for ret, exp in zip(returned, expected):
         for r, e in zip(ret, exp):
