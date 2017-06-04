53e70e2c898471cf9b859af711df63da1cbd4ab9
ostr00000 <ostr00000@gmail.com>
2017-06-04 15:49:21 +0200

    change project struct

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='comparetool',
+      version='1.0',
+      description='compare artice on web',
+      url='https://github.com/AGHPythonCourse2017/zad3-ostr00000',
+      author='ostr00000',
+      license='MIT',
+      packages=['comparetool'],
+      zip_safe=False)
